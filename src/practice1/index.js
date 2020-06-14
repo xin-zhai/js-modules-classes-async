a7d7440b378f09e23e31c981e48eb684fa76e005
zhai xin <xin.zhai@thoughtworks.com>
2020-06-14 22:14:52 +0800

    finish practice1

--- src/practice1/index.js
+++ src/practice1/index.js
@@ -1,1 +1,7 @@
+import { REGION } from "./constant";
+import { getURL } from "./utils";
 
+const url = getURL();
+const region = REGION;
+
+export { url, region };
--- src/practice1/utils.js
+++ src/practice1/utils.js
@@ -1,1 +1,5 @@
+import { ADDRESS, PORT, PATH } from "./constant";
 
+export function getURL() {
+  return `${ADDRESS}:${PORT}${PATH}`;
+}
