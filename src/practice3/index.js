7cfa557ddbc904eb2cbc95d613fb64dabc2d025c
zhai xin <xin.zhai@thoughtworks.com>
2020-06-16 10:21:49 +0800

    finish practice3

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,19 @@
-export const getPoetry
+export const getPoetry = () => {
+  const url = "https://v1.jinrishici.com/all.json";
+  return fetch(url, { method: "GET" })
+    .then((res) => res.json())
+    .then((res) => {
+      const arr = [res.origin, res.author, res.content];
+      return arr;
+    });
+};
+
+// export async function getPoetry() {
+//   const url = "https://v1.jinrishici.com/all.json";
+//   return fetch(url, { method: "GET" })
+//     .then((res) => res.json())
+//     .then((res) => {
+//       const arr = [res.origin, res.author, res.content];
+//       return arr;
+//     });
+// }
